1d574b903f0f6fd02e6092e81a1827895f8b375c
Juan Pablo Monsalve <85030158+jumonsalve7@users.noreply.github.com>
2025-02-16 15:01:33 +0000

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,28 @@
+let countries = [
+    ["Argentina", "Brasil","Uruguay"],
+    ["Bolivia", "Panama", "Colombia"],
+    ["USA", "CANADA", "PIPO"],
+    ["Mexico", "Peru" , "Ecuador"]
+]; 
+
+for(let f = 0; f < countries.length;f++){
+    for(let c = 0; c< countries[f].length;c++){
+        console.log(countries[f][c]);
+    }} 
+countries.push(["PORTUGAL", "POLONIA", "ESPAÑA"]); //agg al final
+countries.pop(); // borra el ultimo
+countries.shift(); // borra el primero
+countries.unshift("ASIA"); // agg al principio 
+*/
+
+let countries = ["Argentina", "PERU","Uruguay"];
+let countries2 = ["Mexico", "Peru" , "Ecuador"];
+
+let allCountries = countries.concat(countries2); //sumar los dos arrays
+console.log(allCountries);
+allCountries.reverse(); // le cambia el sentido de atras para adelante
+console.log(allCountries);
+allCountries.sort(); // ordena por orden alfabetico
+console.log(allCountries);
+let country = countries.filter(country => country.includes("a"));  //filtrar por a
+console.log(country);
